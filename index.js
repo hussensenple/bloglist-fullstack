6c84427d3483eb486c58924601060b7972511e93
Hussien <hussensenple@gmail.com>
2026-04-07 17:46:21 +0200

    Refactor project structure and add user authentication with bcrypt

--- index.js
+++ index.js
@@ -1,37 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const usersRouter = require('./controllers/users'); 
 
 const app = express();
+app.use(express.json()); 
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-});
-
-const Blog = mongoose.model('Blog', blogSchema);
-
+// الاتصال بقاعدة البيانات
 const mongoUrl = 'mongodb://localhost/bloglist';
 mongoose.connect(mongoUrl);
 
-app.use(express.json());
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then(blogs => {
-    response.json(blogs);
-  });
-});
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-  
-  blog.save().then(result => {
-    response.status(201).json(result);
-  });
-});
+app.use('/api/users', usersRouter);
 
 const PORT = 3003;
 app.listen(PORT, () => {
-  console.log('Server running on port 3003');
+  console.log(`Server running on port ${PORT}`);
 });
