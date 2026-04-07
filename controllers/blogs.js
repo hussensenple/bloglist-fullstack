433d0168ba84616f7b86d1757e69ae126fdf1489
Hussien <hussensenple@gmail.com>
2026-04-07 18:27:21 +0200

    Implement sorting and pagination with metadata

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -5,26 +5,62 @@ const User = require('../models/user');
 const blogsRouter = express.Router();
 
 blogsRouter.get('/', async (request, response) => {
-  const searchTerm = request.query.search; 
-  const authorTerm = request.query.author; 
-  
-  let filter = {}; 
+  try {
+    const searchTerm = request.query.search; 
+    const authorTerm = request.query.author; 
+    const sortBy = request.query.sortBy;
+    const order = request.query.order;
+    const page = parseInt(request.query.page) || 1;   
+    const limit = parseInt(request.query.limit) || 10;
 
-  if (searchTerm) {
-    filter.title = { $regex: searchTerm, $options: 'i' };
+    let filter = {}; 
+    if (searchTerm) {
+      filter.title = { $regex: searchTerm, $options: 'i' };
+    }
+    if (authorTerm) {
+      filter.author = { $regex: authorTerm, $options: 'i' };
+    }
+
+    let sortOptions = {};
+    if (sortBy) {
+      const allowedSortFields = ['likes'];
+      if (!allowedSortFields.includes(sortBy)) {
+        return response.status(400).json({ error: 'unsupported sort field' });
+      }
+      
+      const sortOrder = order === 'desc' ? -1 : 1;
+      sortOptions[sortBy] = sortOrder;
+    }
+
+    const skip = (page - 1) * limit;
+
+    const blogs = await Blog.find(filter)
+      .sort(sortOptions)
+      .skip(skip)
+      .limit(limit)
+      .populate('user', { username: 1, name: 1 });
+
+    const totalMatchingBlogs = await Blog.countDocuments(filter);
+    const totalPages = Math.ceil(totalMatchingBlogs / limit);
+
+    response.json({
+      data: blogs,
+      meta: {
+        currentPage: page,
+        pageSize: blogs.length,
+        totalMatchingBlogs: totalMatchingBlogs,
+        totalPages: totalPages
+      }
+    });
+
+  } catch (error) {
+    response.status(500).json({ error: error.message });
   }
-
-  if (authorTerm) {
-    filter.author = { $regex: authorTerm, $options: 'i' };
-  }
-
-  const blogs = await Blog.find(filter).populate('user', { username: 1, name: 1 });
-  response.json(blogs);
 });
+
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body;
-
   const user = await User.findOne();
 
   const blog = new Blog({
@@ -36,7 +72,6 @@ blogsRouter.post('/', async (request, response) => {
   });
 
   const savedBlog = await blog.save();
-
   user.blogs = user.blogs.concat(savedBlog._id);
   await user.save();
 
@@ -46,14 +81,12 @@ blogsRouter.post('/', async (request, response) => {
 blogsRouter.patch('/:id/like', async (request, response) => {
   try {
     const blog = await Blog.findById(request.params.id);
-
     if (!blog) {
       return response.status(404).json({ error: 'blog not found' });
     }
 
     blog.likes = blog.likes + 1;
     const updatedBlog = await blog.save();
-    
     response.status(200).json(updatedBlog);
 
   } catch (error) {
