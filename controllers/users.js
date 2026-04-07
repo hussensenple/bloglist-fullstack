2e71b8f280fbab565b906eeeac1640d92a279f9e
Hussien <hussensenple@gmail.com>
2026-04-07 18:07:20 +0200

    Add user validation and link blogs to user

--- controllers/users.js
+++ controllers/users.js
@@ -7,21 +7,32 @@ const usersRouter = express.Router();
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body;
 
-  const saltRounds = 10;
-  const passwordHash = await bcrypt.hash(password, saltRounds);
+  if (!password || password.length < 3) {
+    return response.status(400).json({
+      error: 'password must be at least 3 characters long'
+    });
+  }
 
-  const user = new User({
-    username: username,
-    name: name,
-    passwordHash: passwordHash,
-  });
+  try {
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(password, saltRounds);
 
-  const savedUser = await user.save();
-  response.status(201).json(savedUser);
+    const user = new User({
+      username: username,
+      name: name,
+      passwordHash: passwordHash,
+    });
+
+    const savedUser = await user.save();
+    response.status(201).json(savedUser);
+    
+  } catch (error) {
+    response.status(400).json({ error: error.message });
+  }
 });
 
 usersRouter.get('/', async (request, response) => {
-  const users = await User.find({});
+  const users = await User.find({}).populate('blogs', { title: 1, author: 1, url: 1 });
   response.json(users);
 });
 
--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const usersRouter = require('./controllers/users'); 
+
+const usersRouter = require('./controllers/users');
+const blogsRouter = require('./controllers/blogs');
 
 const app = express();
-app.use(express.json()); 
+app.use(express.json());
 
-// الاتصال بقاعدة البيانات
 const mongoUrl = 'mongodb://localhost/bloglist';
 mongoose.connect(mongoUrl);
 
 app.use('/api/users', usersRouter);
+app.use('/api/blogs', blogsRouter);
 
 const PORT = 3003;
 app.listen(PORT, () => {
